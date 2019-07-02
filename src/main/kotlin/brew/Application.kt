32ebfe96f2b404f0abe45c9c2726bf8b477daab7
Nikita Eliseenko <nikita.eliseenko@zensoft.io>
2019-07-02 16:37:08 +0300

    first commit

---
+++ src/main/kotlin/brew/Application.kt
@@ -0,0 +1,11 @@
+package brew
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+println("Hello Homebrew!")
+}
